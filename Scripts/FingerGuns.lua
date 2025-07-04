83dc2c214cd1b463ce0e3db363cec92983598f0c
Vajdani <dvajda656@gmail.com>
2025-07-04 23:47:02 +0200

    mod files

---
+++ Scripts/FingerGuns.lua
@@ -0,0 +1,505 @@
+dofile( "$GAME_DATA/Scripts/game/AnimationUtil.lua" )
+dofile( "$SURVIVAL_DATA/Scripts/util.lua" )
+dofile( "$SURVIVAL_DATA/Scripts/game/survival_projectiles.lua" )
+
+
+---@class FingerGuns : ToolClass
+---@field tpAnimations table
+---@field fpAnimations table
+FingerGuns = class()
+
+local renderablesTp = {
+    "$CONTENT_DATA/Tools/char_fingergun_tp.rend"
+}
+
+local renderablesFp = {
+    "$CONTENT_DATA/Tools/char_fingergun_fp.rend"
+}
+
+function FingerGuns:client_onCreate()
+    self.isLocal = self.tool:isLocal()
+
+    self.shootEffect = sm.effect.createEffect( "SpudgunBasic - BasicMuzzel" )
+	self.shootEffectFP = sm.effect.createEffect( "SpudgunBasic - FPBasicMuzzel" )
+
+    self.wantEquipped = false
+	self.equipped = false
+
+    self.shotCount = 0
+end
+
+function FingerGuns.client_onUpdate( self, dt )
+
+	local isSprinting =  self.tool:isSprinting()
+	local isCrouching =  self.tool:isCrouching()
+
+	if self.isLocal then
+		updateFpAnimations( self.fpAnimations, self.equipped, dt )
+	end
+
+	if not self.equipped then
+		if self.wantEquipped then
+			self.wantEquipped = false
+			self.equipped = true
+		end
+		return
+	end
+
+	local effectPos, rot
+    local bone = self:getFingerBone()
+	if self.isLocal then
+		local dir = sm.localPlayer.getDirection()
+		local firePos = self.tool:getFpBonePos( bone )
+
+        effectPos = firePos + dir * 0.2
+		rot = sm.vec3.getRotation( sm.vec3.new( 0, 0, 1 ), dir )
+
+		self.shootEffectFP:setPosition( effectPos )
+		self.shootEffectFP:setVelocity( self.tool:getMovementVelocity() )
+		self.shootEffectFP:setRotation( rot )
+	end
+
+	local pos = self.tool:getTpBonePos( bone )
+	local dir = self.tool:getDirection()
+	effectPos = pos + dir * 0.2
+	rot = sm.vec3.getRotation( sm.vec3.new( 0, 0, 1 ), dir )
+
+	self.shootEffect:setPosition( effectPos )
+	self.shootEffect:setVelocity( self.tool:getMovementVelocity() )
+	self.shootEffect:setRotation( rot )
+
+	-- Timers
+	self.fireCooldownTimer = math.max( self.fireCooldownTimer - dt, 0.0 )
+	self.spreadCooldownTimer = math.max( self.spreadCooldownTimer - dt, 0.0 )
+	self.sprintCooldownTimer = math.max( self.sprintCooldownTimer - dt, 0.0 )
+
+	if self.isLocal then
+		local dispersion = 0.0
+		local fireMode = self.normalFireMode
+		local recoilDispersion = 1.0 - ( math.max( fireMode.minDispersionCrouching, fireMode.minDispersionStanding ) + fireMode.maxMovementDispersion )
+
+		if isCrouching then
+			dispersion = fireMode.minDispersionCrouching
+		else
+			dispersion = fireMode.minDispersionStanding
+		end
+
+		if self.tool:getRelativeMoveDirection():length() > 0 then
+			dispersion = dispersion + fireMode.maxMovementDispersion * self.tool:getMovementSpeedFraction()
+		end
+
+		if not self.tool:isOnGround() then
+			dispersion = dispersion * fireMode.jumpDispersionMultiplier
+		end
+
+		self.movementDispersion = dispersion
+
+		self.spreadCooldownTimer = clamp( self.spreadCooldownTimer, 0.0, fireMode.spreadCooldown )
+		local spreadFactor = fireMode.spreadCooldown > 0.0 and clamp( self.spreadCooldownTimer / fireMode.spreadCooldown, 0.0, 1.0 ) or 0.0
+
+		self.tool:setDispersionFraction( clamp( self.movementDispersion + spreadFactor * recoilDispersion, 0.0, 1.0 ) )
+        self.tool:setCrossHairAlpha( 1.0 )
+        self.tool:setInteractionTextSuppressed( false )
+	end
+
+	-- Sprint block
+	local blockSprint = self.sprintCooldownTimer > 0.0
+	self.tool:setBlockSprint( blockSprint )
+
+	local playerDir = self.tool:getSmoothDirection()
+	local angle = math.asin( playerDir:dot( sm.vec3.new( 0, 0, 1 ) ) ) / ( math.pi / 2 )
+
+	local crouchWeight = self.tool:isCrouching() and 1.0 or 0.0
+	local normalWeight = 1.0 - crouchWeight
+
+	local totalWeight = 0.0
+	for name, animation in pairs( self.tpAnimations.animations ) do
+		animation.time = animation.time + dt
+
+		if name == self.tpAnimations.currentAnimation then
+			animation.weight = math.min( animation.weight + ( self.tpAnimations.blendSpeed * dt ), 1.0 )
+
+			if animation.time >= animation.info.duration - self.blendTime then
+				if animation.nextAnimation ~= "" then
+					setTpAnimation( self.tpAnimations, animation.nextAnimation, 0.001 )
+				end
+			end
+		else
+			animation.weight = math.max( animation.weight - ( self.tpAnimations.blendSpeed * dt ), 0.0 )
+		end
+
+		totalWeight = totalWeight + animation.weight
+	end
+
+	totalWeight = totalWeight == 0 and 1.0 or totalWeight
+	for name, animation in pairs( self.tpAnimations.animations ) do
+		local weight = animation.weight / totalWeight
+		if name == "idle" then
+			self.tool:updateMovementAnimation( animation.time, weight )
+		elseif animation.crouch then
+			self.tool:updateAnimation( animation.info.name, animation.time, weight * normalWeight )
+			self.tool:updateAnimation( animation.crouch.name, animation.time, weight * crouchWeight )
+		else
+			self.tool:updateAnimation( animation.info.name, animation.time, weight )
+		end
+	end
+
+	-- Third Person joint lock
+	local relativeMoveDirection = self.tool:getRelativeMoveDirection()
+	if ( ( ( isAnyOf( self.tpAnimations.currentAnimation, { "aimInto", "aim", "shoot" } ) and ( relativeMoveDirection:length() > 0 or isCrouching) ) or ( --[[self.aiming and]] ( relativeMoveDirection:length() > 0 or isCrouching) ) ) and not isSprinting ) then
+		self.jointWeight = math.min( self.jointWeight + ( 10.0 * dt ), 1.0 )
+	else
+		self.jointWeight = math.max( self.jointWeight - ( 6.0 * dt ), 0.0 )
+	end
+
+	if ( not isSprinting ) then
+		self.spineWeight = math.min( self.spineWeight + ( 10.0 * dt ), 1.0 )
+	else
+		self.spineWeight = math.max( self.spineWeight - ( 10.0 * dt ), 0.0 )
+	end
+
+	local finalAngle = ( 0.5 + angle * 0.5 )
+	self.tool:updateAnimation( "spine_bend", 1 - finalAngle, self.spineWeight )
+
+	local totalOffsetZ = lerp( -22.0, -26.0, crouchWeight )
+	local totalOffsetY = lerp( 6.0, 12.0, crouchWeight )
+	local crouchTotalOffsetX = clamp( ( angle * 60.0 ) -15.0, -60.0, 40.0 )
+	local normalTotalOffsetX = clamp( ( angle * 50.0 ), -45.0, 50.0 )
+	local totalOffsetX = lerp( normalTotalOffsetX, crouchTotalOffsetX , crouchWeight )
+	local finalJointWeight = ( self.jointWeight )
+
+	self.tool:updateJoint( "jnt_hips", sm.vec3.new( totalOffsetX, totalOffsetY, totalOffsetZ ), 0.35 * finalJointWeight * ( normalWeight ) )
+
+	local crouchSpineWeight = ( 0.35 / 3 ) * crouchWeight
+	self.tool:updateJoint( "jnt_spine1", sm.vec3.new( totalOffsetX, totalOffsetY, totalOffsetZ ), ( 0.10 + crouchSpineWeight )  * finalJointWeight )
+	self.tool:updateJoint( "jnt_spine2", sm.vec3.new( totalOffsetX, totalOffsetY, totalOffsetZ ), ( 0.10 + crouchSpineWeight ) * finalJointWeight )
+	self.tool:updateJoint( "jnt_spine3", sm.vec3.new( totalOffsetX, totalOffsetY, totalOffsetZ ), ( 0.45 + crouchSpineWeight ) * finalJointWeight )
+	self.tool:updateJoint( "jnt_head", sm.vec3.new( totalOffsetX, totalOffsetY, totalOffsetZ ), 0.3 * finalJointWeight )
+
+
+	self.tool:updateCamera( 2.8, 30.0, sm.vec3.new( 0.65, 0.0, 0.05 ), 0 )
+	self.tool:updateFpCamera( 30.0, sm.vec3.new( 0.0, 0.0, 0.0 ), 0, 1 )
+end
+
+function FingerGuns:client_onEquip()
+	self.wantEquipped = true
+	self.jointWeight = 0.0
+
+	local currentRenderablesTp = {}
+	local currentRenderablesFp = {}
+
+	for k,v in pairs( renderablesTp ) do currentRenderablesTp[#currentRenderablesTp+1] = v end
+	for k,v in pairs( renderablesFp ) do currentRenderablesFp[#currentRenderablesFp+1] = v end
+
+    self.tool:setTpRenderables( currentRenderablesTp )
+	if self.isLocal then
+		self.tool:setFpRenderables( currentRenderablesFp )
+    end
+
+	self:loadAnimations()
+
+	setTpAnimation( self.tpAnimations, "equip", 0.0001 )
+	if self.isLocal then
+		swapFpAnimation( self.fpAnimations, "unequip", "equip", 0.2 )
+	end
+end
+
+function FingerGuns:client_onUnequip()
+	self.wantEquipped = false
+	self.equipped = false
+	if sm.exists( self.tool ) then
+		setTpAnimation( self.tpAnimations, "unequip" )
+		if self.tool:isLocal() then
+			self.tool:setMovementSlowDown( false )
+			self.tool:setBlockSprint( false )
+			self.tool:setCrossHairAlpha( 1.0 )
+			self.tool:setInteractionTextSuppressed( false )
+			if self.fpAnimations.currentAnimation ~= "unequip" then
+				swapFpAnimation( self.fpAnimations, "equip", "unequip", 0.2 )
+			end
+		end
+	end
+end
+
+function FingerGuns:client_onEquippedUpdate(lmb, rmb, f)
+    if lmb == 1 or lmb == 2 then
+        self:cl_onPrimaryUse()
+    end
+
+    return true, false
+end
+
+function FingerGuns:sv_n_onShoot()
+	self.network:sendToClients( "cl_n_onShoot" )
+end
+
+function FingerGuns:cl_n_onShoot()
+	if not self.tool:isLocal() and self.tool:isEquipped() then
+		self:onShoot()
+	end
+end
+
+function FingerGuns:onShoot()
+    self.shotCount = (self.shotCount + 1) % 2
+    local anim = self.shotCount == 0 and "shootright" or "shootleft"
+
+	setTpAnimation( self.tpAnimations, anim, 10.0 )
+    if self.isLocal then
+        setFpAnimation( self.fpAnimations, anim, 0.05 )
+    end
+
+	if self.tool:isInFirstPersonView() then
+		self.shootEffectFP:start()
+	else
+		self.shootEffect:start()
+	end
+end
+
+function FingerGuns:cl_onPrimaryUse()
+	if self.tool:getOwner().character == nil then
+		return
+	end
+
+	if self.fireCooldownTimer <= 0.0 then
+
+		if not sm.game.getEnableAmmoConsumption() or sm.container.canSpend( sm.localPlayer.getInventory(), obj_plantables_potato, 1 ) then
+			local firstPerson = self.tool:isInFirstPersonView()
+
+			local dir = sm.localPlayer.getDirection()
+
+			local firePos = self:calculateFirePosition()
+			local fakePosition = self:calculateTpMuzzlePos()
+			local fakePositionSelf = fakePosition
+			if firstPerson then
+				fakePositionSelf = self:calculateFpMuzzlePos()
+			end
+
+			-- Aim assist
+			if not firstPerson then
+				local raycastPos = sm.camera.getPosition() + sm.camera.getDirection() * sm.camera.getDirection():dot( GetOwnerPosition( self.tool ) - sm.camera.getPosition() )
+				local hit, result = sm.localPlayer.getRaycast( 250, raycastPos, sm.camera.getDirection() )
+				if hit then
+					local norDir = sm.vec3.normalize( result.pointWorld - firePos )
+					local dirDot = norDir:dot( dir )
+
+					if dirDot > 0.96592583 then -- max 15 degrees off
+						dir = norDir
+					else
+						local radsOff = math.asin( dirDot )
+						dir = sm.vec3.lerp( dir, norDir, math.tan( radsOff ) / 3.7320508 ) -- if more than 15, make it 15
+					end
+				end
+			end
+
+			dir = dir:rotate( math.rad( 0.955 ), sm.camera.getRight() ) -- 50 m sight calibration
+
+			-- Spread
+			local fireMode = self.normalFireMode
+			local recoilDispersion = 1.0 - ( math.max(fireMode.minDispersionCrouching, fireMode.minDispersionStanding ) + fireMode.maxMovementDispersion )
+
+			local spreadFactor = fireMode.spreadCooldown > 0.0 and clamp( self.spreadCooldownTimer / fireMode.spreadCooldown, 0.0, 1.0 ) or 0.0
+			spreadFactor = clamp( self.movementDispersion + spreadFactor * recoilDispersion, 0.0, 1.0 )
+			local spreadDeg =  fireMode.spreadMinAngle + ( fireMode.spreadMaxAngle - fireMode.spreadMinAngle ) * spreadFactor
+
+			dir = sm.noise.gunSpread( dir, spreadDeg )
+
+			local owner = self.tool:getOwner()
+			if owner then
+				sm.projectile.projectileAttack( projectile_potato, 28, firePos, dir * fireMode.fireVelocity, owner, fakePosition, fakePositionSelf )
+			end
+
+			self.fireCooldownTimer = fireMode.fireCooldown
+			self.spreadCooldownTimer = math.min( self.spreadCooldownTimer + fireMode.spreadIncrement, fireMode.spreadCooldown )
+			self.sprintCooldownTimer = self.sprintCooldown
+
+			self:onShoot()
+			self.network:sendToServer( "sv_n_onShoot", dir )
+		else
+			local fireMode = self.normalFireMode
+			self.fireCooldownTimer = fireMode.fireCooldown
+			sm.audio.play( "PotatoRifle - NoAmmo" )
+		end
+	end
+end
+
+
+-- #region purgatory
+function FingerGuns:calculateFirePosition()
+	local crouching = self.tool:isCrouching()
+	local firstPerson = self.tool:isInFirstPersonView()
+	local dir = sm.localPlayer.getDirection()
+	local pitch = math.asin( dir.z )
+	local right = sm.localPlayer.getRight()
+
+	local fireOffset = sm.vec3.new( 0.0, 0.0, 0.0 )
+
+	if crouching then
+		fireOffset.z = 0.15
+	else
+		fireOffset.z = 0.45
+	end
+
+	if firstPerson then
+        fireOffset = fireOffset + right * 0.05
+	else
+		fireOffset = fireOffset + right * 0.25
+		fireOffset = fireOffset:rotate( math.rad( pitch ), right )
+	end
+	local firePosition = GetOwnerPosition( self.tool ) + fireOffset
+	return firePosition
+end
+
+function FingerGuns:calculateTpMuzzlePos()
+	local crouching = self.tool:isCrouching()
+	local dir = sm.localPlayer.getDirection()
+	local pitch = math.asin( dir.z )
+	local right = sm.localPlayer.getRight()
+	local up = right:cross(dir)
+
+	local fakeOffset = sm.vec3.new( 0.0, 0.0, 0.0 )
+
+	--General offset
+	fakeOffset = fakeOffset + right * 0.25
+	fakeOffset = fakeOffset + dir * 0.5
+	fakeOffset = fakeOffset + up * 0.25
+
+	--Action offset
+	local pitchFraction = pitch / ( math.pi * 0.5 )
+	if crouching then
+		fakeOffset = fakeOffset + dir * 0.2
+		fakeOffset = fakeOffset + up * 0.1
+		fakeOffset = fakeOffset - right * 0.05
+
+		if pitchFraction > 0.0 then
+			fakeOffset = fakeOffset - up * 0.2 * pitchFraction
+		else
+			fakeOffset = fakeOffset + up * 0.1 * math.abs( pitchFraction )
+		end
+	else
+		fakeOffset = fakeOffset + up * 0.1 *  math.abs( pitchFraction )
+	end
+
+	local fakePosition = fakeOffset + GetOwnerPosition( self.tool )
+	return fakePosition
+end
+
+function FingerGuns:calculateFpMuzzlePos()
+	local fovScale = ( sm.camera.getFov() - 45 ) / 45
+
+	local up = sm.localPlayer.getUp()
+	local dir = sm.localPlayer.getDirection()
+	local right = sm.localPlayer.getRight()
+
+	local muzzlePos45 = sm.vec3.new( 0.0, 0.0, 0.0 )
+	local muzzlePos90 = sm.vec3.new( 0.0, 0.0, 0.0 )
+
+	muzzlePos45 = muzzlePos45 - up * 0.15
+	muzzlePos45 = muzzlePos45 + right * 0.2
+	muzzlePos45 = muzzlePos45 + dir * 1.25
+
+    muzzlePos90 = muzzlePos90 - up * 0.15
+	muzzlePos90 = muzzlePos90 + right * 0.2
+	muzzlePos90 = muzzlePos90 + dir * 0.25
+
+	return self.tool:getFpBonePos( self:getFingerBone() ) + sm.vec3.lerp( muzzlePos45, muzzlePos90, fovScale )
+end
+
+function FingerGuns:loadAnimations()
+	self.tpAnimations = createTpAnimations(
+		self.tool,
+		{
+			shootleft = { "fingergun_shootleft", { nextAnimation = "idle" } },
+			shootright = { "fingergun_shootright", { nextAnimation = "idle" } },
+			idle = { "fingergun_idle" },
+			equip = { "fingergun_equip", { nextAnimation = "idle" } },
+			unequip = { "fingergun_unequip" }
+		}
+	)
+
+	local movementAnimations = {
+		idle = "fingergun_idle",
+		-- idleRelaxed = "fingergun_relax",
+
+		sprint = "fingergun_sprint",
+		runFwd = "fingergun_run_fwd",
+		runBwd = "fingergun_run_bwd",
+
+		-- jump = "fingergun_jump",
+		-- jumpUp = "fingergun_jump_up",
+		-- jumpDown = "fingergun_jump_down",
+
+		-- land = "fingergun_jump_land",
+		-- landFwd = "fingergun_jump_land_fwd",
+		-- landBwd = "fingergun_jump_land_bwd",
+
+		-- crouchIdle = "fingergun_crouch_idle",
+		-- crouchFwd = "fingergun_crouch_fwd",
+		-- crouchBwd = "fingergun_crouch_bwd"
+	}
+
+	for name, animation in pairs( movementAnimations ) do
+		self.tool:setMovementAnimation( name, animation )
+	end
+
+	if self.isLocal then
+		self.fpAnimations = createFpAnimations(
+			self.tool,
+			{
+                shootleft = { "fingergun_shootleft", { nextAnimation = "idle" } },
+                shootright = { "fingergun_shootright", { nextAnimation = "idle" } },
+				idle = { "fingergun_idle", { looping = true } },
+				equip = { "fingergun_equip", { nextAnimation = "idle" } },
+				unequip = { "fingergun_unequip" },
+			}
+		)
+	end
+
+	self.normalFireMode = {
+		fireCooldown = 0.20,
+		spreadCooldown = 0.18,
+		spreadIncrement = 2.6,
+		spreadMinAngle = .25,
+		spreadMaxAngle = 8,
+		fireVelocity = 130.0,
+
+		minDispersionStanding = 0.1,
+		minDispersionCrouching = 0.04,
+
+		maxMovementDispersion = 0.4,
+		jumpDispersionMultiplier = 2
+	}
+
+	self.aimFireMode = {
+		fireCooldown = 0.20,
+		spreadCooldown = 0.18,
+		spreadIncrement = 1.3,
+		spreadMinAngle = 0,
+		spreadMaxAngle = 8,
+		fireVelocity =  130.0,
+
+		minDispersionStanding = 0.01,
+		minDispersionCrouching = 0.01,
+
+		maxMovementDispersion = 0.4,
+		jumpDispersionMultiplier = 2
+	}
+
+	self.fireCooldownTimer = 0.0
+	self.spreadCooldownTimer = 0.0
+
+	self.movementDispersion = 0.0
+
+	self.sprintCooldownTimer = 0.0
+	self.sprintCooldown = 0.3
+
+	self.aimBlendSpeed = 3.0
+	self.blendTime = 0.2
+
+	self.jointWeight = 0.0
+	self.spineWeight = 0.0
+end
+
+function FingerGuns:getFingerBone()
+    return self.shotCount == 0 and "jnt_left_handindex4" or "jnt_right_handindex4"
+end
+-- #endregion
